2b7028ca4107ec104d1a43962584dd940e69185f
Ning <a24701770@gmail.com>
2024-04-13 18:51:12 +0800

    add: get method and route not found default method

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder, Result};
+use serde::Serialize;
+
+#[derive(Serialize)]
+pub struct Response {
+    pub message: String,
+}
+
+#[get("/todo")]
+async fn todo_list() -> impl Responder {
+    let response = Response {
+        message: "everythind is well done".to_string(),
+    };
+
+    HttpResponse::Ok().json(response)
+}
+
+async fn not_found() -> Result<HttpResponse> {
+    let response = Response {
+        message: "Resource not found".to_string(),
+    };
+    Ok(HttpResponse::NotFound().json(response))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(todo_list)
+            .default_service(web::route().to(not_found))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
